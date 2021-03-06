4e23dcbc885b87ac5b051a5022d68a9800d288d5
mygs <saito.milton@gmail.com>
2021-03-06 09:17:43 -0300

    book weighted bid/ask price

--- AggregatedBookAnalytics.py
+++ AggregatedBookAnalytics.py
@@ -6,38 +6,52 @@ from prettytable import PrettyTable
 class AggregatedBookAnalytics:
     def __init__(self, book):
         self.book = book
-        self.spread = None
-        self.best_bid_price = None
-        self.best_ask_price = None
-        self.pressure_ask = None
-        self.pressure_bid = None
+        self.spread = 0
+        self.best_bid_price = 0
+        self.best_ask_price = 0
+        self.pressure_ask = 0
+        self.pressure_bid = 0
+        self.weighted_bid_price = 0
+        self.weighted_ask_price = 0
+        self.weighted_price = 0
         self.calc()
 
     def calc(self):
         book_length = len(self.book.bid)
-        amount_bid = 0
-        amount_ask = 0
+        volume_bid = 0
+        volume_ask = 0
+        total_qty_bid = 0
+        total_qty_ask = 0
+
         for i in range(book_length):
             bid = self.book.bid[i]
             ask = self.book.ask[i]
-            amount_bid = bid["Q"] * bid["P"]
-            amount_ask = ask["Q"] * ask["P"]
+            volume_bid = volume_bid + bid["Q"] * bid["P"]
+            volume_ask = volume_ask + ask["Q"] * ask["P"]
+            total_qty_bid = total_qty_bid + bid["Q"]
+            total_qty_ask = total_qty_ask + ask["Q"]
 
         self.best_bid_price = self.book.bid[0]["P"]
         self.best_ask_price = self.book.ask[0]["P"]
         self.spread =  self.best_ask_price - self.best_bid_price
-        amount_total = amount_ask + amount_bid
-        self.pressure_ask = 100*amount_ask/amount_total
-        self.pressure_bid = 100*amount_bid/amount_total
+        volume_total = volume_ask + volume_bid
+        self.pressure_ask = 100*volume_ask/volume_total
+        self.pressure_bid = 100*volume_bid/volume_total
+        self.weighted_bid_price = volume_bid/total_qty_bid
+        self.weighted_ask_price = volume_ask/total_qty_ask
+        self.weighted_price = (volume_bid + volume_ask)/(total_qty_bid + total_qty_ask)
 
     def print(self):
         summary = PrettyTable()
         summary.field_names = ["ANALYTIC", "RESULT"]
-        summary.add_row([ "Spread", self.spread])
-        summary.add_row([ "Best Bid Price", self.best_bid_price])
-        summary.add_row([ "Best Ask Price", self.best_ask_price])
-        summary.add_row([ "Bid pressure", self.pressure_bid])
-        summary.add_row([ "Ask pressure", self.pressure_ask])
+        summary.add_row([ "Bid/Ask Spread $", '{0:.2f}'.format(self.spread)])
+        summary.add_row([ "Weighted Price $", '{0:.2f}'.format(self.weighted_price)])
+        summary.add_row([ "Best Bid $", self.best_bid_price])
+        summary.add_row([ "Best Ask $", self.best_ask_price])
+        summary.add_row([ "Balance Bid %", '{0:.2f}'.format(self.pressure_bid)])
+        summary.add_row([ "Balance Ask %", '{0:.2f}'.format(self.pressure_ask)])
+        summary.add_row([ "Weighted Bid $", '{0:.2f}'.format(self.weighted_bid_price)])
+        summary.add_row([ "Weighted Ask $", '{0:.2f}'.format(self.weighted_ask_price)])
         print(summary)
 
 
